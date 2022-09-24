6a6a6e25dbcb876c1269abd2d4d1f04899f9316b
pdcourses <freshitpt@gmail.com>
2022-09-24 10:50:22 +0300

    async validation

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,28 +1,25 @@
 const {userCreateSchema, userUpdateSchema} = require("../utils/validationSchemas");
 
-module.exports.validateUserCreate = (req, res, next) => {
+module.exports.validateUserCreate =  async (req, res, next) => {
   const { body } = req;
-  userCreateSchema
-    .validate(body)
-    .then((validatedUser) => {
-      req.body = validatedUser;
-      next();
-    })
-    .catch((error) => {
+  try{
+    const validatedUser = await userCreateSchema.validate(body);
+    req.body = validatedUser;
+    next();  
+  }catch(error){
       res.status(400).send(error);
-    });
+  }
 };
 
-module.exports.validateUserUpdate = (req, res, next) => {
+module.exports.validateUserUpdate = async (req, res, next) => {
   const { body } = req;
-  userUpdateSchema
-    .validate(body)
-    .then((validatedUser) => {
+  try{
+      const validatedUser = await userUpdateSchema.validate(body);
       req.body = validatedUser;
       next();
-    })
-    .catch((error) => {
+  }
+  catch(error){
       res.status(400).send(error);
-    });
+  }
 };
 
